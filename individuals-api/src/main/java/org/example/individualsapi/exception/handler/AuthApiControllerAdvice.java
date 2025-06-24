5d4149a225e560db51e4384f9336ff3ea6aba73c
Georgiy Demerchyan <Georgi747d@gmail.com>
2025-06-24 22:45:32 +0300

    feat: User Registration

---
+++ individuals-api/src/main/java/org/example/individualsapi/exception/handler/AuthApiControllerAdvice.java
@@ -0,0 +1,21 @@
+package org.example.individualsapi.exception.handler;
+
+import org.example.individualsapi.exception.KeycloakApiException;
+import org.example.individualsapi.model.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class AuthApiControllerAdvice {
+
+    @ExceptionHandler(KeycloakApiException.class)
+    public ResponseEntity<?> handleKeycloakApiException(KeycloakApiException ex) {
+        ErrorResponse errorResponse = ex.getErrorResponse();
+
+        return ResponseEntity
+                .status(HttpStatus.valueOf(errorResponse.getStatus()))
+                .body(errorResponse.getError());
+    }
+}
---
+++ individuals-api/src/main/java/org/example/individualsapi/service/KeycloakService.java
@@ -0,0 +1,137 @@
+package org.example.individualsapi.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.individualsapi.exception.KeycloakApiException;
+import org.example.individualsapi.model.KeycloakError;
+import org.example.individualsapi.model.KeycloakUserCredentials;
+import org.example.individualsapi.model.KeycloakUserRequest;
+import org.example.individualsapi.model.dto.ErrorResponse;
+import org.example.individualsapi.model.dto.TokenResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.function.Function;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class KeycloakService {
+
+    private final WebClient webClient;
+
+    @Value("${keycloak.client_id}")
+    private String clientId;
+
+    @Value("${keycloak.client_secret}")
+    private String clientSecret;
+
+    @Value("${keycloak.realm_name}")
+    private String realmName;
+
+    @Value("${keycloak.admin_username}")
+    private String adminUsername;
+
+    @Value("${keycloak.admin_password}")
+    private String adminPassword;
+
+    public Mono<TokenResponse> getUserToken(String username, String password) {
+        log.info("Getting user token for {}", username);
+
+        MultiValueMap<String, String> formData = createTokenRequestBody(username, password);
+
+        return webClient.post()
+                .uri(String.format("/realms/%s/protocol/openid-connect/token",realmName))
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                .body(BodyInserters.fromFormData(formData))
+                .retrieve()
+                .onStatus(httpStatusCode ->
+                        httpStatusCode.is4xxClientError() ||
+                                httpStatusCode.is5xxServerError(),
+                        keycloakErrorHandler()
+                )
+                .bodyToMono(TokenResponse.class)
+                .doOnSuccess(_ -> log.info("Token for user {} successfully received", username))
+                .doOnError(throwable -> log.error("Error in try to get token for user: {} -> {}",username, throwable.getMessage()));
+    }
+
+    public Mono<Void> addNewUser(String email, String password, String username, String adminToken) {
+
+        KeycloakUserRequest requestBody = KeycloakUserRequest.builder()
+                .email(email)
+                .username(username)
+                .enabled(true)
+                .emailVerified(true)
+                .firstName(username)
+                .lastName(username)
+                .credentials(
+                        List.of(
+                                KeycloakUserCredentials
+                                        .builder()
+                                        .temporary(false)
+                                        .type("password")
+                                        .value(password)
+                                        .build()
+                        )
+                )
+                .build();
+
+        log.info("Adding new user {}", email);
+        log.debug("Request body {}", requestBody);
+
+        return webClient.post()
+                .uri(String.format("admin/realms/%s/users",realmName))
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + adminToken)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .body(BodyInserters.fromValue(requestBody))
+                .retrieve()
+                .onStatus(httpStatusCode ->
+                                httpStatusCode.is4xxClientError() ||
+                                httpStatusCode.is5xxServerError(),
+                        keycloakErrorHandler())
+                .bodyToMono(Void.class)
+                .doOnSuccess(_ -> log.info("User {} successfully added", email))
+                .doOnError(throwable -> log.error("Error in try to add new user: {} -> {}", email, throwable.getMessage()));
+    }
+
+    public Mono<TokenResponse> getAdminToken() {
+        return getUserToken(adminUsername, adminPassword);
+    }
+
+
+    private MultiValueMap<String, String> createTokenRequestBody(String username, String password) {
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("grant_type", "password");
+        formData.add("username", username);
+        formData.add("password", password);
+        formData.add("client_id", clientId);
+        formData.add("client_secret", clientSecret);
+        return formData;
+    }
+
+    private Function<ClientResponse, Mono<? extends Throwable>> keycloakErrorHandler() {
+        return clientResponse ->
+                clientResponse.bodyToMono(KeycloakError.class)
+                        .flatMap(error -> {
+                            String message = String.format("%s: %s", error.getError(), error.getErrorDescription());
+
+                            ErrorResponse errorResponse = new ErrorResponse();
+                            errorResponse.setStatus(clientResponse.statusCode().value());
+                            errorResponse.setError(message);
+
+                            return Mono.error(new KeycloakApiException(errorResponse, message));
+                        });
+    }
+
+}
