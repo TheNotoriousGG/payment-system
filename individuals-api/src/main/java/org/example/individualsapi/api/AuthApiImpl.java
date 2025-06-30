ac557a57a514b35eecc0f45f03a971e4c7e25713
Georgiy Demerchyan <Georgi747d@gmail.com>
2025-06-30 11:59:39 +0300

    Реализация spring security

--- individuals-api/src/main/java/org/example/individualsapi/api/AuthApiImpl.java
+++ individuals-api/src/main/java/org/example/individualsapi/api/AuthApiImpl.java
@@ -1,14 +1,11 @@
 package org.example.individualsapi.api;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.individualsapi.model.dto.*;
-import org.example.individualsapi.service.KeycloakService;
 import org.example.individualsapi.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.Validator;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,7 +22,6 @@ public class AuthApiImpl implements AuthApi {
 
     private final UserService userService;
 
-
     @Override
     public Mono<ResponseEntity<TokenResponse>> authLoginPost(Mono<UserLoginRequest> userLoginRequest, ServerWebExchange exchange) {
         return userLoginRequest
@@ -36,7 +32,7 @@ public class AuthApiImpl implements AuthApi {
 
     @Override
     public Mono<ResponseEntity<UserInfoResponse>> authMeGet(ServerWebExchange exchange) {
-        return null;
+        return userService.getAuthUserInfoFromContext().map(ResponseEntity::ok);
     }
 
     @Override
---
+++ individuals-api/src/main/java/org/example/individualsapi/util/AuthContextUtil.java
@@ -0,0 +1,17 @@
+package org.example.individualsapi.util;
+
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimAccessor;
+import reactor.core.publisher.Mono;
+
+public class AuthContextUtil {
+
+    public static Mono<String> getCurrentUserId() {
+        return ReactiveSecurityContextHolder.getContext()
+                .map(SecurityContext::getAuthentication)
+                .map(auth -> (Jwt) auth.getPrincipal())
+                .map(JwtClaimAccessor::getSubject);
+    }
+}
