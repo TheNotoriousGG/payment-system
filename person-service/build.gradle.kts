77f122288a29b7eca2404feea584d8365cfe92c7
Georgiy Demerchyan <Georgi747d@gmail.com>
2025-10-03 12:39:39 +0300

    Добавлен слой инфраструктуры

--- person-service/build.gradle.kts
+++ person-service/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.5.5"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.openapi.generator") version "7.15.0"
 }
 
 group = "org.example"
@@ -34,6 +35,32 @@ dependencies {
     testImplementation("io.projectreactor:reactor-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("org.hibernate:hibernate-envers:7.1.1.Final")
+}
+
+openApiGenerate {
+    generatorName.set("spring")
+    inputSpec.set("$projectDir/src/main/resources/static/openapi/person-service.yaml")
+    outputDir.set(layout.buildDirectory.dir("generated/sources/openapi").get().asFile.absolutePath)
+    apiPackage.set("org.example.personservice.controller")
+    modelPackage.set("org.example.personservice.model.dto")
+    configOptions.set(mapOf(
+        "interfaceOnly" to "true",
+        "skipDefaultInterface" to "true",
+        "useJakartaEe" to "true",
+        "reactive" to "true"
+    ))
+}
+
+sourceSets {
+    main {
+        java {
+            srcDir(layout.buildDirectory.dir("generated/sources/openapi/src/main/java"))
+        }
+    }
+}
+
+tasks.compileJava {
+    dependsOn("openApiGenerate")
 }
 
 tasks.withType<Test> {
--- person-service/src/main/java/org/example/personservice/controller/PersonController.java
+++ person-service/src/main/java/org/example/personservice/controller/PersonController.java
@@ -1,4 +1,14 @@
 package org.example.personservice.controller;
 
-public class PersonController {
+import org.example.personservice.model.dto.UserRegistrationRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+public class PersonController implements PersonApi{
+
+    @Override
+    public Mono<ResponseEntity<String>> personRegistrationPost(Mono<UserRegistrationRequest> userRegistrationRequest, ServerWebExchange exchange) {
+        return null;
+    }
 }
